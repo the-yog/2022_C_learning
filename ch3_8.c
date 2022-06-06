8e875cf087c29a7c959600cd38dc33ad210c739f
seoyeon lee(yog) <106678254+the-yog@users.noreply.github.com>
2022-06-06 21:33:06 +0900

    Create ch3_8.c

---
+++ ch3_8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+  char s1[] = "I am";
+  char s2[] = " your F...";
+  puts(s1);
+  puts(s2);
+  printf("%s", s1);
+  printf("%s", s2);
+}
+
+/* puts(변수); 의 형식으로 사용하는 puts()함수는,
+문자열을 화면에 출력하는 기능을 하는데
+출력하고나서 자동으로 줄이 바뀐다! \n 사요나라! */
