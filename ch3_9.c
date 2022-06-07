c290c0dad8c60e61f6d0f2509ecd34436fa73e9a
seoyeon lee(yog) <106678254+the-yog@users.noreply.github.com>
2022-06-07 15:40:40 +0900

    Create ch3_9.c

---
+++ ch3_9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+
+  int x, y;   // x, y 값은 정수
+  
+  x=10; 
+  y=3;
+  
+  printf("x + y = %d \n", x + y);
+  
+  printf("x / y = %d \n", x / y);
+  
+  printf("x %% y = %d \n", x % y);    // %문자 출력하려고 %문자를 2개 연속으로 입력
+  
+  printf("y %% x = %d \n", y % x);
+
+}
